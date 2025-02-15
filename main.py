49af525f02f788c80433ba4d1d882379572180d2
VenturaVini <vini.ventura98@gmail.com>
2025-02-14 21:52:02 -0300

    ajusta da main com as env

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from routes import auth, produtos
 
@@ -6,11 +7,18 @@ app = FastAPI()
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(produtos.router, tags=["Produtos"])
 
+@app.get("/env")
+def read_env():
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    CHAT_ID = os.getenv("CHAT_ID")
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    return {
+        "BOT_TOKEN": BOT_TOKEN,
+        "CHAT_ID": CHAT_ID,
+        "SECRET_KEY": SECRET_KEY
+    }
+
 if __name__ == "__main__":
+    port = int(os.getenv("PORT", 7000))
     import uvicorn
-    import os
-    #uvicorn.run(app, host="0.0.0.0", port=7000)
-
-
-    port = int(os.getenv("PORT", 7000))  # Usa a porta definida pelo Railway
     uvicorn.run(app, host="0.0.0.0", port=port)
