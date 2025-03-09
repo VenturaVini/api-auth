7d14a31494c02b0fba424bfb6e11d352e0a8c0e1
VenturaVini <vini.ventura98@gmail.com>
2025-03-08 22:47:12 -0300

    ops

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,20 @@
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # Importar o middleware
 from routes import auth, produtos
 
 app = FastAPI()
 
+# Adicionando o middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todas as origens (cuidado em produção)
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos HTTP (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permite todos os headers
+)
+
+# Incluindo as rotas
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(produtos.router, tags=["Produtos"])
 
@@ -19,6 +30,6 @@ def read_env():
     }
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 7000))
+    port = int(os.getenv("PORT", 7200))
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=port)
